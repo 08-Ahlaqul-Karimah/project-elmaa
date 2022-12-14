417ce027e2a1d20e124eb22e35fb5f999b964fdb
08-Ahlaqul-Karimah <83933498+08-Ahlaqul-Karimah@users.noreply.github.com>
2022-12-14 22:54:36 +0700

    Update index.py

--- index.py
+++ index.py
@@ -94,10 +94,10 @@ with preprocessing:
     - min = nilai minimum semua data asli
     - max = nilai maksimum semua data asli
     """)
-    df = df.drop(columns=['id',])
+    df = df.drop(columns=['id', 'dual_sim', 'four_g', 'three_g', 'touch_screen', 'wifi'])
     #Mendefinisikan Varible X dan Y
     X = df[['battery_power', 'clock_speed', 'fc', 'int_memory', 'm_dep', 'mobile_wt', 'n_cores', 'pc', 'px_height', 'px_width', 'ram', 'sc_h', 'sc_w', 'talk_time']]
-    y = df['blue','dual_sim', 'four_g', 'three_g', 'touch_screen', 'wifi' ].values
+    y = df['blue' ].values
     df
     X
     df_min = X.min()
