edca58968ddc948fa9268bcb1b1a7b3f2df18141
08-Ahlaqul-Karimah <83933498+08-Ahlaqul-Karimah@users.noreply.github.com>
2022-12-14 22:56:59 +0700

    Update index.py

--- index.py
+++ index.py
@@ -97,7 +97,7 @@ with preprocessing:
     df = df.drop(columns=['id', 'dual_sim', 'four_g', 'three_g', 'touch_screen', 'wifi'])
     #Mendefinisikan Varible X dan Y
     X = df[['battery_power', 'clock_speed', 'fc', 'int_memory', 'm_dep', 'mobile_wt', 'n_cores', 'pc', 'px_height', 'px_width', 'ram', 'sc_h', 'sc_w', 'talk_time']]
-    y = df['blue' ].values
+    y = df['blue'].values
     df
     X
     df_min = X.min()
