343b67e8ee1ba2a17b55d0adacf1ee474bc73a90
08-Ahlaqul-Karimah <83933498+08-Ahlaqul-Karimah@users.noreply.github.com>
2022-12-14 22:55:29 +0700

    Update index.py

--- index.py
+++ index.py
@@ -116,7 +116,7 @@ with preprocessing:
     st.write(scaled_features)
 
     st.subheader('Target Label')
-    dumies = pd.get_dummies(df.Species).columns.values.tolist()
+    dumies = pd.get_dummies(df.blue).columns.values.tolist()
     dumies = np.array(dumies)
 
     labels = pd.DataFrame({
